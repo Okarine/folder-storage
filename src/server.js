392f266618726b9bee6237ccb79c16850dac59c5
Okarine <olesiakarin@gmail.com>
2025-02-14 14:47:39 +0200

    breadcrumbs

--- src/server.js
+++ src/server.js
@@ -6,30 +6,28 @@ const port = 3000;
 
 const baseDir = path.join(__dirname, '../public/data/SW mods 1.6.8');
 
-// Маршрут для отображения главной страницы и передачи списка папок
-app.get('/', (req, res) => {
-    // Читаем HTML-шаблон
+function renderDirPage(dirPath, res) {
+    // Reading the HTML template
     const indexFile = path.join(__dirname, 'index.html');
-    
-    // Читаем содержимое директории
-    fs.readdir(baseDir, { withFileTypes: true }, (err, files) => {
+
+     // Reading the contents of the directory
+    fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
         if (err) {
             return res.status(500).send('Error reading directory');
         }
 
         const fileList = files.map(file => ({
             name: file.name,
-            path: path.join(req.query.path || '', file.name),
+            path: path.join(file.name),
             isDirectory: file.isDirectory()
         }));
-
-        // Читаем HTML-файл и встраиваем туда переменную с JSON-данными
+        // We read the HTML file and embed a variable with JSON data into it
         fs.readFile(indexFile, 'utf8', (err, htmlData) => {
             if (err) {
                 return res.status(500).send('Error reading HTML file');
             }
 
-            // Встраиваем JSON прямо в HTML как глобальную переменную
+            // Embedding JSON directly into HTML as a global variable
             const modifiedHtml = htmlData.replace(
                 '<!-- FOLDER_LIST_PLACEHOLDER -->',
                 `<script>const fileList = ${JSON.stringify(fileList)};</script>`
@@ -38,12 +36,32 @@ app.get('/', (req, res) => {
             res.send(modifiedHtml);
         });
     });
+}
+
+// Route for the main page (root folder)
+app.get('/', (req, res) => {
+    renderDirPage(baseDir, res);
 });
 
-// Статическая отдача файлов (если нужно для других файлов, например стилей)
+// Dynamic route for folders
+app.get('/*', (req, res) => {
+    const relativePath = req.params[0]; // Extracting the path from the route parameter
+    const dirPath = path.join(baseDir, relativePath);
+
+    // Checking if a path exists
+    fs.stat(dirPath, (err, stats) => {
+        if (err || !stats.isDirectory()) {
+            return res.status(404).send('Directory not found');
+        }
+
+        renderDirPage(dirPath, res);
+    });
+})
+
+// Static file delivery
 app.use(express.static(__dirname));
 
-// Запуск сервера
+// Starting the server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
